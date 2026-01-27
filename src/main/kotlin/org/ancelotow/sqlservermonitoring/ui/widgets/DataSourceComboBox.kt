70d4433038294a18a7b3fb996341dc68d40c5e43
Ancelotow <ancelotow@gmail.com>
2026-01-27 23:01:40 +0100

    refactor: Remove unused refresh button and update plugin metadata (tool window name and icon)

---
+++ src/main/kotlin/org/ancelotow/sqlservermonitoring/ui/widgets/DataSourceComboBox.kt
@@ -0,0 +1,41 @@
+package org.ancelotow.sqlservermonitoring.ui.widgets
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.intellij.database.dataSource.LocalDataSource
+import org.jetbrains.jewel.ui.component.CircularProgressIndicator
+import org.jetbrains.jewel.ui.component.ListComboBox
+
+
+@Composable
+fun DataSourceComboBox(
+    modifier: Modifier = Modifier,
+    items: List<LocalDataSource>,
+    selected: LocalDataSource?,
+    isLoading: Boolean,
+    onItemSelected: (LocalDataSource) -> Unit
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ListComboBox(
+            modifier = Modifier.weight(1f),
+            enabled = !isLoading && items.isNotEmpty(),
+            items = items.map { it.name },
+            selectedIndex = items.indexOf(selected),
+            onSelectedItemChange = { onItemSelected(items[it]) }
+        )
+
+        Spacer(Modifier.width(8.dp))
+
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(16.dp)
+            )
+        }
+    }
+}
